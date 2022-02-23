dff35da8a7e41ad4e255c51cadb26e6018e415da
minggui.han <minggui.han@ivydad.com>
2022-02-23 17:30:49 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: ['eslint-config-airbnb-base', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'import/extensions': 'off',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error', { ignoreTypeReferences: true }],
+    'no-prototype-builtins': 'off',
+    'import/prefer-default-export': 'off',
+    'no-console': 'off'
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.ts'],
+        moduleDirectory: ['node_modules', './src'],
+      },
+    },
+  },
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+}
