5672d80f3537e98de7d5ea1b519adf3b5eeb2c73
阿duang <minggui.han@ivydad.com>
2022-03-01 01:44:23 +0800

    after test close prisma

--- src/test/unit/persistence/cache.spec.ts
+++ src/test/unit/persistence/cache.spec.ts
@@ -2,22 +2,26 @@ import { expect } from 'chai';
 import _ from 'underscore';
 import { PrismaClient, traveler } from '@prisma/client';
 import cache from '../../../persistence/cache';
-import { Traveler } from '../../../types';
+// import { Traveler } from '../../../types';
 
 describe('cache', () => {
   const prisma = new PrismaClient();
-  prisma.$connect();
-  let minId: number;
 
   before(async () => {
-    const minTraveler = await prisma.traveler.findFirst({ orderBy: { id: 'asc' } });
-    if (!minTraveler) {
-      throw Error('table traveler is empty');
-    }
-    minId = minTraveler.id;
+    await prisma.$connect();
   });
 
   describe('#getTravelerById()', () => {
+    let minId: number;
+
+    before(async () => {
+      const minTraveler = await prisma.traveler.findFirst({ orderBy: { id: 'asc' } });
+      if (!minTraveler) {
+        throw Error('table traveler is empty');
+      }
+      minId = minTraveler.id;
+    });
+
     it('input minId - 1 return null', async () => {
       const result = await cache.getTravelerById(minId - 1);
       expect(result).to.eq(null);
@@ -27,5 +31,9 @@ describe('cache', () => {
       expect(result).not.to.eq(null);
       expect(result?.id).to.eq(minId);
     });
+  });
+
+  after(async () => {
+    await prisma.$disconnect();
   });
 });
